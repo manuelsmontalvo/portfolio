523e5bf6ce71bde92a6d7171ac110e1f6d49d66f
Manuel  Montalvo <manny@Manuels-MacBook-Pro.local>
2021-05-01 21:37:48 -0400

    deployment

--- client/src/components/aboutme/aboutme.jsx
+++ client/src/components/aboutme/aboutme.jsx
@@ -5,7 +5,7 @@ import {Bio} from "./bio";
 
 const Splash = styled.div`
   display: flex;
-  min-width: 1010px; // No default x-axis scroll
+  min-width: 100vw; // No default x-axis scroll
   height: 600px;
   justify-content: center;
   align-items: center;
