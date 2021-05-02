73d99a00ef403d930955ed4692d70a4813f50d4e
Manuel  Montalvo <manny@Manuels-MacBook-Pro.local>
2021-05-01 23:01:19 -0400

    deployment

--- client/src/components/aboutme/bio.jsx
+++ client/src/components/aboutme/bio.jsx
@@ -6,30 +6,30 @@ const AboutContainer = styled.div`
   display: flex;
   flex-flow: column;
   box-sizing: border-box;
-  width: 100%;
-  padding: 100px;
-  height: 600px;
-  margin-top:5px;
   justify-content: center;
   align-items: center;
+  width: 100%;
+  height: 600px;
+  margin-top: 5px;
+  padding: 100px;
   background-color: #f8f0dc;
 `;
 
 const Title = styled.h2`
-  font-size: 35px;
   margin: 10px 0 20px 0;
+  color: #454241;
+  font-size: 35px;
   font-weight: 600;
   font-family: "open sans";
-  color: #454241;
 `;
 
 const About = styled.p`
   margin: 0;
+  color: #454241;
+  text-align: center;
   font-size: 35px;
   font-weight: 500;
   font-family: "open sans";
-  color: #454241;
-  text-align: center;
 `;
 
 export function Bio() {
