41cf22b2086ac2a000686567a869a29296a00d9e
Manuel  Montalvo <manny@Manuels-MacBook-Pro.local>
2021-05-01 20:56:04 -0400

    deployment

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import AboutMe  from "./components/aboutme/aboutme";
-import Nav from "./components/header/nav/Nav.jsx";
+import Nav from "./components/header/nav/Nav";
 import "./App.css";
 
 function App() {
