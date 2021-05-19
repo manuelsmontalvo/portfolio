5b9e4cdb62ebc7f13831ce6e3b2440ff0daaefd1
Manuel  Montalvo <manny@Manuels-MacBook-Pro.local>
2021-05-18 21:53:41 -0600

    refactor

--- client/src/components/projects/project.jsx
+++ client/src/components/projects/project.jsx
@@ -7,10 +7,10 @@ const ProjectContainer = styled.div`
   flex-flow: column;
   box-sizing: border-box;
   position: relative;
-  justify-content: space-around;
+  justify-content: center;
   padding-bottom: 10%;
   width: 100%;
-  height: 900px;
+  height: fit-content;
   background-color: #454241;
 `;
 
@@ -20,6 +20,7 @@ const Title = styled.h2`
   font-weight: 600;
   font-family: "open sans";
   margin-top: 50px;
+  margin-bottom: 30px;
 
   @media (max-width: 778px) {
     font-size: 25px;
@@ -30,7 +31,7 @@ const MainContainer = styled.div`
   display: flex;
   flex-flow: row wrap;
   justify-content: center;
-  gap: 300px;
+  gap: 100px;
 
   @media (max-width: 778px) {
     gap: 25px;
@@ -39,31 +40,31 @@ const MainContainer = styled.div`
 
 export default function Project() {
   return (
-      <ProjectContainer id="projects">
-        <Title>Projects</Title>
-        <MainContainer>
-          <Projectcard
-            Site="https://elegant-allen-e97c72.netlify.app/"
-            Repo="https://github.com/manuelsmontalvo/github-finder"
-            Title="GitHub Finder"
-            Description="This is a Github profile application made with Reactjs using the GitHub API. It utilizes context hooks for state management "
-            Img="https://imgur.com/vCrO26u"
-          />
-          <Projectcard
-            Site="https://collaboreatery.netlify.app/"
-            Repo="https://github.com/manuelsmontalvo/collaboreatery"
-            Title="Collaboreatery"
-            Description="Collboreatery is community fed database for restaraunts based price, location, and cuisine. This apllication was made with React, Express, MongoDB, and NodeJS."
-            Img="https://imgur.com/SKioqik.png"
-          />
-          <Projectcard
-            Site="https://www.manuelsmontalvo.com/"
-            Repo="https://github.com/manuelsmontalvo/portfolio"
-            Title="Portfolio Site"
-            Description="This portfolio site was made with React, Styled components, Framer Motion, and other animations made from scratch."
-            Img="https://imgur.com/J4Dtgtd.png"
-          />
-        </MainContainer>
-      </ProjectContainer>
+    <ProjectContainer id='projects'>
+      <Title>Projects</Title>
+      <MainContainer>
+        <Projectcard
+          Site='https://elegant-allen-e97c72.netlify.app/'
+          Repo='https://github.com/manuelsmontalvo/github-finder'
+          Title='GitHub Finder'
+          Description='This is a Github profile application made with Reactjs using the GitHub API. It utilizes context hooks for state management '
+          Img='https://imgur.com/vCrO26u.png'
+        />
+        <Projectcard
+          Site='https://collaboreatery.netlify.app/'
+          Repo='https://github.com/manuelsmontalvo/collaboreatery'
+          Title='Collaboreatery'
+          Description='Collboreatery is community fed database for restaraunts based price, location, and cuisine. This apllication was made with React, Express, MongoDB, and NodeJS.'
+          Img='https://imgur.com/SKioqik.png'
+        />
+        <Projectcard
+          Site='https://www.manuelsmontalvo.com/'
+          Repo='https://github.com/manuelsmontalvo/portfolio'
+          Title='Portfolio Site'
+          Description='This portfolio site was made with React, Styled components, Framer Motion, and other animations made from scratch.'
+          Img='https://imgur.com/J4Dtgtd.png'
+        />
+      </MainContainer>
+    </ProjectContainer>
   );
 }
