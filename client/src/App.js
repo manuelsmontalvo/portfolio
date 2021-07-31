0cf9e8032b06626cc12ee1112167b0bf00797fc8
Manuel  Montalvo <manny@Manuels-MacBook-Pro.local>
2021-07-31 15:58:06 -0400

    revamp start

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,6 @@
-import AboutMe from "./components/aboutme/aboutme";
-import Nav from "./components/nav/nav";
-import { Bio } from "./components/aboutme/bio";
-import Footer from "./components/footer/footer";
-import "./App.css";
-import Project from "./components/projects/project";
-import ContactMe from "./components/contactme/contactme";
-
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <AboutMe />
-      <Bio />
-      <Project />
-      <ContactMe/>
-      <Footer />
+    <div className='App'>
     </div>
   );
 }
