cb97eb2db063eb960919b2b0a1a15c29bb00010c
Manuel  Montalvo <manny@Manuels-MacBook-Pro.local>
2021-08-01 21:29:50 -0400

    s

--- client/src/components/ContactMe.js
+++ client/src/components/ContactMe.js
@@ -18,12 +18,12 @@ export default function ContactMe() {
 
   return (
     <div className="">
-    <h1 style={{ textShadow: "2px 4px 3px rgba(0,0,0,0.3)" }} className="pt-52 ml-28 mb-16 py-10 justify-center text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+    <h1 id="contact-me" style={{ textShadow: "2px 4px 3px rgba(0,0,0,0.3)" }} className="pt-20 ml-28 mb-16 py-10 justify-center text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
       <span id="projects" className="block text-indigo-400 xl:inline">
         Contact Me
       </span>
     </h1>
-    <form className="contact-form w-full flex flex-col item-center  px-56 pb-40 justify-center  " id="contact-form" onSubmit={sendEmail}>
+    <form className="contact-form w-full flex flex-col item-center  px-56 pb-24 justify-center  " id="contact-form" onSubmit={sendEmail}>
         <div className="flex fle-row">
           <div className="flex flex-wrap item-center justify-center">
             <div className="w-full  px-3">
@@ -67,7 +67,7 @@ export default function ContactMe() {
         </div>
         <div className="item-center justify-center mt-4 flex w-full">
           <div className="item-center justify-center  flex w-full md:w-1/3">
-            <button type="submit" value="Send" className="shadow bg-indigo-400 mb-16 hover:bg-indigo-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">
+            <button type="submit" value="Send" className="shadow bg-indigo-400 mb-4 hover:bg-indigo-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">
               Send
             </button>
           </div>
